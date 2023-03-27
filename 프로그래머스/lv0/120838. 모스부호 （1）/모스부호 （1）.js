162f6f03c40e266ad72a260739ffd876482d3adb
Simon <74960684+nhk9710@users.noreply.github.com>
2023-03-27 16:35:34 +0900

    [level 0] Title: 모스부호 (1), Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,15 @@
+function solution(letter) {
+    var answer = [];
+    let morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+    }
+    let morseArr = letter.split(' ')
+    morseArr.map(v => {
+        answer.push(morse[`${v}`])
+    })
+    return answer = answer.join('');
+}
