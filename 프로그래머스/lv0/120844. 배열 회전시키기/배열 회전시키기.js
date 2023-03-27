fcd4a8fea0bf6d42f29080555499ab61842a1480
Simon <74960684+nhk9710@users.noreply.github.com>
2023-03-27 16:33:41 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -0,0 +1,12 @@
+
+const solution = (numbers, direction) => {
+    let answer = numbers
+
+    if(direction === 'right') {
+      answer.unshift(answer.pop())  
+    } else {
+      answer.push(answer.shift())
+    }
+    
+    return answer
+}
