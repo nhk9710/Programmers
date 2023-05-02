8d9a40891ce3ce4e0ff7f6ee342ae4258854e04b
Simon <74960684+nhk9710@users.noreply.github.com>
2023-05-02 16:25:39 +0900

    [level 2] Title: 짝지어 제거하기, Time: 51.37 ms, Memory: 46.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,8 @@
+function solution(s){
+    const arr = [];
+    for (let i = 0; i < s.length; i++) {
+        if (arr.length > 0 && arr[arr.length-1] === s[i]) arr.pop();
+        else arr.push(s[i]);
+    }
+    return arr.length === 0 ? 1 : 0;
+}
